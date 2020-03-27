efa3fef7f18e978b09fb14400d26e0df81e8517e
AbhinavDRP <54456815+AbhinavDRP@users.noreply.github.com>
2020-03-27 17:24:26 +0530

    Add files via upload

---
+++ ML_POC/ML_Based_Invoice_Prediction/GlobalVariables.cs
@@ -0,0 +1,8 @@
+﻿/// <summary>
+/// Global variables Class
+/// </summary>
+public static class GlobalVariables
+{
+    public static string trainingDataFilePath = @"/Users/abhinav.dasiga/Projects/ML_POC/Data Sets/TrainingData.txt";
+    public static string testDataFilePath = @"/Users/abhinav.dasiga/Projects/ML_POC/Data Sets/TestData.txt";
+}
---
+++ ML_POC/ML_Based_Invoice_Prediction/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace ML_Based_Invoice_Prediction
+{
+    /// <summary>
+    /// Main Entry Point of the Program
+    /// </summary>
+    class Program
+    {
+        /// <summary>
+        /// The entry point of the program, where the program control starts and ends.
+        /// </summary>
+        /// <param name="args">The command-line arguments.</param>
+        static void Main(string[] args)
+        {
+            ML_Model classObj = new ML_Model();
+            classObj.Execute_ML_Model();
+        }
+    }
+}
+ 
